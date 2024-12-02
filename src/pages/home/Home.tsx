5a2489a927a2964276537f485840b3b63689e451
Demetri65 <demetri.lopez1599@gmail.com>
2024-12-01 20:53:52 -0500

    initial website under construction

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,22 @@
+// import { useNavigate } from "react-router-dom";
+import "./Home.css"
+
+export const Home = () => {
+    // const navigate = useNavigate();
+    return (
+      <>
+      <div className="parent">
+        <div className="div1 card"><h1>Under Construction</h1></div>
+        <div className="div2 card"> </div>
+        <div className="div3 card"> </div>
+        <div className="div4 card"> </div>
+        <div className="div5 card"> </div>
+        <div className="div6 card"> </div>
+        <div className="div7 card"> </div>
+        <div className="div8 card"> </div>
+      </div>
+        
+      </>
+       
+      );
+    };
