f35b2568ddd2b4e74ac1350c3b54d34035e1b2cf
Demetri65 <demetri.lopez1599@gmail.com>
2024-12-01 20:53:52 -0500

    initial website under construction

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+// import { useNavigate } from "react-router-dom";
+
+export const Home = () => {
+    // const navigate = useNavigate();
+    return (
+      <>
+        <div className="div1 card"><h1>Under Construction</h1></div>
+      </>
+       
+      );
+    };
